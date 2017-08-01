1043956ec094517b7674972c38909ecd75c9380e
f1she3 <f1sher@gmx.com>
2017-08-01 23:42:01 +0200

    Finishing reply system

--- functions/article.func.php
+++ functions/article.func.php
@@ -49,75 +49,48 @@ function comment_infos($comment_id){
 		return $infos;
 	}
 }
-function display_article($input_id){
+function display_comments($article_id, $page_id){
 	$mysqli = get_link();
-	$infos = article_infos($input_id);
-	if(!$infos){
-		set_error('Erreur 404', 'zoom-out', 'L\'article que vous recherchez n\'éxiste pas', 'home');
-	}else{
-		$fdate = date_create($infos['date']);
-		$fdate = date_format($fdate, 'G:i, \l\e j/m Y');
-		$user_infos = user_infos($infos['author']);
-		echo 	"<div class=\"page-header\">
-				<h2 class=\"text-center\">".$infos['title']."</h2>
-			</div>";
-		echo "<ul class=\"breadcrumb\">
-			<li><a href=\"".constant('BASE_URL')."category&cat".$infos['category']."\">".$infos['category']."</a></li>
-				<li>".$infos['title']."</li>
-			</ul>";
-		echo 	"<h4 class=\"text-left\">
-				<img src=\"../css/images/account_black.svg\" height=\"40\" width=\"40\">
-				<a href=\"".constant('BASE_URL')."profile&user=".$infos['author']."\"><abbr title=\"".$user_infos."\">".$infos['author']."</abbr></a>
-				".$fdate."
-			</h4>";
-		echo 	"<pre><p class=\"\">".$infos['content']."</p></pre>";
-		if(get_rank($_SESSION['name']) > 0){
-		echo 	"<form method=\"POST\">
-				<div class=\"form-group\">
-					<button name=\"close_article\" class=\"btn btn-danger\">
-						<span class=\"glyphicon glyphicon-ban-circle\"></span> 
-					</button>
-					<button name=\"close_article\" class=\"btn btn-info\">
-						<span class=\"glyphicon glyphicon-ban-circle\"></span> 
-					</button>
-				</div>
-			</form><hr>";
-			
-		}
-		display_comments($input_id);
-		echo 	"<form method=\"POST\">
-				<div class=\"form-group col-md-8 col-md-offset-2\">
-					<textarea class=\"form-control\" style=\"resize:none\" name=\"comment\"></textarea>
-				</div>
-				<div class=\"form-group col-md-4 col-md-offset-4\">
-					<button class=\"btn btn-primary col-sm-2 col-xs-2 col-xs-offset-5\">
-						<span class=\"glyphicon glyphicon-send\"></span> 
-					</button>
-				</div>
-			</form>";
-	}
-}
-function display_comments($parent_id){
+	$query = mysqli_prepare($mysqli, 'SELECT COUNT(*) FROM comments WHERE article_id = ?');
+	mysqli_stmt_bind_param($query, 'i', $article_id);
+	mysqli_stmt_execute($query);
+	mysqli_stmt_bind_result($query, $count);
+	mysqli_stmt_fetch($query);
+	$per_page = 8;
+	$page_count = ceil($count / $per_page);
+	$position = ($page_id - 1) * $per_page;
 	$mysqli = get_link();
-	$query = mysqli_prepare($mysqli, 'SELECT * FROM  comments WHERE article_id = ?');
-	mysqli_stmt_bind_param($query, 'i', $parent_id);
+	$query = mysqli_prepare($mysqli, 'SELECT * FROM  comments WHERE article_id = ? LIMIT ?, ?');
+	mysqli_stmt_bind_param($query, 'iii', $article_id, $position, $per_page);
 	mysqli_stmt_execute($query);
 	mysqli_stmt_bind_result($query, $id, $parent_id, $author, $content, $reply_to, $date);
+	if($page_id > 1){
+		$infos = article_infos($article_id);
+		echo 	"<div class=\"page-header\">
+				<h2 class=\"text-center\">".$infos['title']."</h2>
+			</div>
+			<ul class=\"breadcrumb\">
+				<li><a href=\"".constant('BASE_URL')."category&cat".$infos['category']."\">".$infos['category']."</a></li>
+				<li><a href=\"".constant('BASE_URL')."article&id=".$article_id."\">".$infos['title']."</a></li>
+				<li>".$page_id."</li>
+			</ul>";
+	}
 	while(mysqli_stmt_fetch($query)){
-		$parent_comment = comment_infos($parent_id);
+		$parent_comment = comment_infos($id);
 		$fdate = date_create($date);
 		$fdate = date_format($fdate, 'G:i, \l\e j/m Y');
 		$user_infos = user_infos($author);
 		if($reply_to == 0){
-			echo 	"<p>".$parent_comment['content']."</p>
+			echo 	"<pre>".bb_decode($parent_comment['content'])."</pre>
 				<h4 class=\"text-left\">
 					<img src=\"../css/images/account_black.svg\" height=\"40\" width=\"40\">
 					<a href=\"".constant('BASE_URL')."profile&user=".$author."\"><abbr title=\"".$user_infos."\">".$author."</abbr></a>
 					".$fdate."
-				</h4><hr>";
+				</h4>
+				<hr>";
 			
 		}else{
-			echo "<blockquote>
+			echo 	"<blockquote>
 					".$parent_comment['content']."
 					<footer>".$parent_comment['author']."</footer>
 				</blockquote>
@@ -129,4 +102,87 @@ function display_comments($parent_id){
 				</h4><hr>";
 		}
 	}
+	if($page_count > 1){
+		echo "<ul class=\"pagination\">";
+		for($i = 1; $i <= $page_count; $i++){
+			if($i == $page_id){
+				echo "<li class=\"active\"><a href=\"".constant('BASE_URL')."article&id=".$article_id."&pid=".$i."\">".$i."</a></li>";
+			}else{
+				echo "<li><a href=\"".constant('BASE_URL')."article&id=".$article_id."&pid=".$i."\">".$i."</a></li>";
+			}
+		}
+		echo 	"</ul>";
+	}
+}
+function display_article($input_id, $page_id){
+	$mysqli = get_link();
+	$infos = article_infos($input_id);
+	if(!$infos){
+		set_error('Erreur 404', 'zoom-out', 'L\'article que vous recherchez n\'éxiste pas', 'home');
+	}else{
+		$fdate = date_create($infos['date']);
+		$fdate = date_format($fdate, 'G:i, \l\e j/m Y');
+		$user_infos = user_infos($infos['author']);
+		echo 	"<div class=\"page-header\">
+				<h2 class=\"text-center\">".$infos['title']."</h2>
+			</div>";
+		echo 	"<ul class=\"breadcrumb\">
+				<li><a href=\"".constant('BASE_URL')."category&cat".$infos['category']."\">".$infos['category']."</a></li>
+				<li>".$infos['title']."</li>
+			</ul>";
+		echo 	"<h4 class=\"text-left\">
+				<img src=\"../css/images/account_black.svg\" height=\"40\" width=\"40\">
+				<a href=\"".constant('BASE_URL')."profile&user=".$infos['author']."\">
+					<abbr title=\"".$user_infos."\">".$infos['author']."</abbr>
+				</a>
+				".$fdate."
+			</h4>";
+		echo "<form method=\"POST\">
+					<button name=\"send_mp\" class=\"btn btn-info\" value=\"1\">
+						<span class=\"glyphicon glyphicon-send\"> mp</span> 
+					</button>";
+		echo 		"</form>
+			<pre>
+				<p>".$infos['content']."</p>
+			</pre>";
+		if(get_rank($_SESSION['name']) > 0){
+			echo 	"<form method=\"POST\">
+						<button name=\"close_article\" class=\"btn btn-primary\">
+							<span class=\"glyphicon glyphicon-share\"> répondre</span>
+						</button>
+						<button name=\"like\" class=\"btn btn-success\">
+							<span class=\"glyphicon glyphicon-thumbs-up\"></span>
+						</button>
+						<button name=\"dislike\" class=\"btn btn-warning\">
+							<span class=\"glyphicon glyphicon-thumbs-down\"></span>
+						</button>";
+
+			$user_rank = get_rank($infos['author']);
+			$my_rank = get_rank($_SESSION['name']);
+			$ranks = get_rank_list();
+			if($my_rank == $ranks['max'] || $my_rank > $user_rank){
+				echo 		"<button name=\"close_article\" class=\"btn btn-danger pull-right\">
+							<span class=\"glyphicon glyphicon-ban-circle\"> fermer</span> 
+						</button>";
+			}
+			echo "	</form><hr>";
+		}
+		display_comments($input_id, $page_id);
+		echo 	"<form method=\"POST\">
+				<div class=\"form-group col-md-8 col-md-offset-2\">
+					<textarea class=\"form-control\" style=\"resize:none\" name=\"comment\" maxlength=\"500\" required></textarea>
+				</div>
+				<div class=\"form-group col-md-4 col-md-offset-4\">
+					<button class=\"btn btn-primary col-sm-2 col-xs-2 col-xs-offset-5\" name=\"submit_comment\">
+						<span class=\"glyphicon glyphicon-send\"></span> 
+					</button>
+				</div>
+			</form>";
+	}
+}
+function post_comment($parent_id, $author, $content, $reply_to){
+	$mysqli = get_link();
+	$query = mysqli_prepare($mysqli, 'INSERT INTO comments (article_id, author, content, reply_to, date) VALUES (?, ?, ?, ?, NOW())');
+	mysqli_stmt_bind_param($query, 'isss', $parent_id, $author, $content, $reply_to);
+	mysqli_stmt_execute($query);
 }
--- pages/article.php
+++ pages/article.php
@@ -1,8 +1,31 @@
 <?php
 
 if(isset($_GET['id']) && !empty($_GET['id']) && ctype_digit($_GET['id'])){
+	if(isset($_GET['pid']) && !empty($_GET['pid']) && ctype_digit($_GET['pid'])){
+		$pid = $_GET['pid'] = secure($_GET['pid']);
+	}else{
+		$pid = 1;	
+	}
 	$id = $_GET['id'] = secure($_GET['id']);
-	display_article($id);
+	if(isset($_POST['submit_comment'])){
+		if(isset($_POST['comment']) && !empty($_POST['comment']) && is_string($_POST['comment']) && strlen($_POST['comment']) <= 500){
+			$comment = $_POST['comment'] = secure($_POST['comment']);
+			post_comment($id, $_SESSION['name'], $comment, 0);
+			display_article($id, $pid);
+		}else{
+			if($pid != 1){
+				display_comments($id, $pid);
+			}else{
+				display_article($id, $pid);
+			}
+		}
+	}else{
+		if($pid != 1){
+			display_comments($id, $pid);
+		}else{
+			display_article($id, $pid);
+		}
+	}
 }else{
 	set_error('Erreur 404', 'zoom-out', 'L\'article que vous recherchez n\'éxiste pas', 'home');
 }
