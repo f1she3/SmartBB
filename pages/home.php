7dcdf7b11f25e0df900555bdcbc46e6a3e2740ab
f1she3 <f1sher@gmx.com>
2017-08-04 23:32:36 +0200

    Security fixes

--- pages/home.php
+++ pages/home.php
@@ -2,14 +2,18 @@
 
 if(isset($_POST['create_category'])){
 	if(isset($_POST['category_name']) && !empty($_POST['category_name']) && is_string($_POST['category_name'])){
-		$category_name = $_POST['category_name']	= secure($_POST['category_name']);
-		if(!is_category($category_name)){
-			create_category($category_name);
+		$ranks = get_rank_list();
+		if(check_rank($_SESSION['name']), $ranks['max']){
+			$category_name = $_POST['category_name']	= secure($_POST['category_name']);
+			if(!is_category($category_name)){
+				create_category($category_name);
+			}
 		}
 	}
-}else{
-	if(isset($_POST['delete_category']) && !empty($_POST['delete_category']) && is_string($_POST['delete_category'])){
-		$delete_category = $_POST['delete_category'] = secure($_POST['delete_category']);
+}else if(isset($_POST['delete_category']) && !empty($_POST['delete_category']) && is_string($_POST['delete_category'])){
+	$delete_category = $_POST['delete_category'] = secure($_POST['delete_category']);
+	$ranks = get_rank_list();
+	if(check_rank($_SESSION['name']), $ranks['max']){
 		if(is_category($delete_category)){
 			delete_category($delete_category);
 		}
