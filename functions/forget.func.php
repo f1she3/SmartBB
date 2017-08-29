3d8db0ad4fb8cb875314c70dfffd6723092d0e16
f1sher <f1sher@gmx.com>
2017-08-29 22:50:46 +0200

    Adding password recovering

--- functions/forget.func.php
+++ functions/forget.func.php
@@ -1,15 +1,160 @@
 <?php
 
-function code_gen(){
-	$tmp_code = random_int(100000000000000000, 999999999999999999);
+// Pseudo random strings of max 64 chars
+function code_gen($max){
+	if($max > 64){
+		$max = 64;
+	}
+	// Decrement, so that there are exactly $max chars
+	$max--;
+	$first = pow(10, $max);
+	$last = '';
+	for($i = 0; $i <= $max; $i++){
+		$last .= '9';
+	}
+	// Pow returns float
+	$first = intval($first);
+	// String to int
+	$last = intval($last);
+	$tmp_code = random_int($first, $last);
 	$code = sha1($tmp_code);
-	$code = substr($code, 0, 6);
+	// Increment for correct value at substr
+	$max++;
+	$code = substr($code, 0, $max);
 
 	return $code;
 }
-function set_recovery_code($code, $user){
+function set_recovery_codes($user, $recovery_code, $token){
 	$mysqli = get_link();
-	$query = mysqli_prepare($mysqli, 'UPDATE users SET recovery_code = ? WHERE BINARY name = ?');
-	mysqli_stmt_bind_param($query, 'ss', $code, $user);
+	$query = mysqli_prepare($mysqli, 'UPDATE users SET recovery_code = ?, token = ? WHERE BINARY name = ?');
+	mysqli_stmt_bind_param($query, 'sss', $recovery_code, $token, $user);
 	mysqli_stmt_execute($query);
+}
+function is_token($token){
+	$mysqli = get_link();
+	$query = mysqli_prepare($mysqli, 'SELECT id FROM users WHERE BINARY token = ?');
+	mysqli_stmt_bind_param($query, 's', $token);
+	mysqli_stmt_execute($query);
+	$i = 0;
+	while(mysqli_stmt_fetch($query)){
+		$i++;
+	}
+	if($i === 0){
+		return false;
+	}else{
+		return true;
+	}
+}
+function check_codes($token, $code){
+	$mysqli = get_link();
+	$query = mysqli_prepare($mysqli, 'SELECT name FROM users WHERE BINARY token = ? AND BINARY recovery_code = ?');
+	mysqli_stmt_bind_param($query, 'ss', $token, $code);
+	mysqli_stmt_execute($query);
+	mysqli_stmt_bind_result($query, $name);
+	$i = 0;
+	while(mysqli_stmt_fetch($query)){
+		$i++;
+	}
+	if($i === 0){
+		return NULL;
+	}else{
+		return $name;
+	}
+}
+function display_confirm_email_form($type, $error, $style){
+	echo 	"<div class=\"page-header\">
+			<h3 class=\"text-center\">Mot de passe oublié</h3>
+		</div>
+		<div class=\"col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3\">";
+	if(!empty($error)){
+		echo 	"<div class=\"alert alert-".$type." text-center\">
+				<span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span> ".$error."
+			</div>";
+	}
+	if(!empty($style)){
+		echo "<div class=\"alert alert-".$type." invisible\">".$style."</div>";
+	}
+	echo	"</div>
+		<div class=\"col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3\">
+			<div class=\"panel panel-default\">
+				<div class=\"panel-heading\">
+					<h3 class=\"panel-title\">
+						<strong>Réinitialiser mon mot de passe</strong>
+					</h3>
+				</div>
+				<div class=\"panel-body\">
+					<form method=\"POST\">
+						<div class=\"form-group\">
+							<label>Votre adresse email :</label>
+							<input type=\"email\" class=\"form-control\" name=\"email\" maxlength=\"40\" 
+								placeholder=\"exemple@exemple.com\" autofocus required>
+						</div>
+						<button class=\"btn btn-primary\" name=\"submit_email\">Confirmer</button>
+						<a class=\"btn btn-default\" href=\"".get_root_url().get_base_url()."login\">
+							<span class=\"glyphicon glyphicon-remove\"></span>
+							Annuler
+						</a>
+					</form>
+				</div>
+			</div>
+		</div>";
+}
+function display_confirm_code_form(){
+	echo 	"<div class=\"page-header\">
+			<h3 class=\"text-center\">Votre code de sécurité</h3>
+		</div>
+		<div class=\"col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3\">
+			<div class=\"panel panel-default\">
+				<div class=\"panel-heading\">
+					<h3 class=\"panel-title\">
+						<strong>Code de sécurité</strong>
+					</h3>
+				</div>
+				<div class=\"panel-body\">
+					<form method=\"POST\">
+						<div class=\"form-group\">
+							<label>Veuillez saisir votre code :</label>
+							<input type=\"text\" class=\"form-control\" name=\"code\" maxlength=\"32\" 
+								placeholder=\"*****************\" autofocus required>
+						</div>
+						<button class=\"btn btn-success center-block\" name=\"submit_code\">
+						<span class=\"glyphicon glyphicon-lock\"></span>
+							Valider
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>";
+}
+function display_new_password_form(){
+	echo 	"<div class=\"page-header\">
+			<h3 class=\"text-center\">Réinitialiser votre mot de passe</h3>
+		</div>
+		<div class=\"col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3\">
+			<div class=\"panel panel-default\">
+				<div class=\"panel-heading\">
+					<h3 class=\"panel-title\">
+						<strong>Nouveau mot de passe</strong>
+					</h3>
+				</div>
+				<div class=\"panel-body\">
+					<form method=\"POST\">
+						<div class=\"form-group\">
+							<label>Nouveau mot de passe :</label>
+							<input type=\"password\" class=\"form-control\" name=\"new_password\" maxlength=\"60\" 
+								placeholder=\"*****************\" autofocus required>
+						</div>
+						<div class=\"form-group\">
+							<label>Répétez le mot de passe :</label>
+							<input type=\"password\" class=\"form-control\" name=\"repeat_new_password\" maxlength=\"60\" 
+								placeholder=\"*****************\" autofocus required>
+						</div>
+						<button class=\"btn btn-primary center-block\" name=\"submit_password\">
+							<span class=\"glyphicon glyphicon-ok\"></span>
+							Réinitialiser
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>";
 }
--- pages/forget.php
+++ pages/forget.php
@@ -1,11 +1,11 @@
 <?php
 
-if(!isset($_SESSION['attempts'])){
-	$_SESSION['attempts'] = 0;
-}
 $error = '';
 $style = 'a';
-if(isset($_POST['submit'])){
+$max_attempts = 3;
+$_SESSION['code_validate'] = 0;
+$_SESSION['token_validate'] = 0;
+if(isset($_POST['submit_email'])){
 	$style = '';
 	if(isset($_POST['email']) && !empty($_POST['email']) && is_string($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
 		$email = $_POST['email'] = secure($_POST['email']);
@@ -15,55 +15,130 @@ if(isset($_POST['submit'])){
 			$user_infos = get_user_infos($is_user);
 			if($email_hash === $user_infos['email']){
 				$code = code_gen(9);
-				set_recovery_code($code, $is_user);
-				mail($is_user, get_project_name().' | réinitialiser votre mot de passe', 'Voici votre code : '.$code);
+				$token = code_gen(32);
+				set_recovery_codes($is_user, $code, $token);
+				$subject = get_project_name().' | Votre code';
+				$to = $email;
+				$from = $name = get_project_name();
+				$headers = 'MIME-Version: 1.0'."\r\n";
+				$headers .= 'Content-type:text/html;charset=UTF-8'."\r\n";
+				$headers .= 'Date: ' . date('r', time())."\r\n";
+				$headers .= 'From: "' . $name . '" <' . $from . '>'."\r\n";
+				$headers .= 'Return-Path: "' . $name . '" <' . $from . '>'."\r\n";
+				$headers .= 'X-Mailer: PHP ' . phpversion()."\r\n";
+				$headers .= 'X-Priority: 2'."\r\n";
+				$headers .= 'X-MSMail-Priority: High'."\r\n";
+				$headers .= 'X-Originating-IP: ' . $_SERVER['SERVER_ADDR']."\r\n";
+				$content = 	"<!DOCTYPE html>
+							<html lang=\"en\">
+								<head>
+									<meta charset=\"utf-8\">
+									<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+									<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+									<meta name=\"description\" content=\"Forum francophone de cyber-sécurité\">
+									<meta name=\"author\" content=\"f1she3\">
+									<link rel=\"icon\" href=\"".get_root_url().'/css/images/favicon.ico'."\">
+									<link href=\"".get_root_url().'/css/bootstrap.min.css'."\" rel=\"stylesheet\">
+									<link href=\"".get_root_url().'/css/style.css'."\" rel=\"stylesheet\">
+								</head>
+								<body>
+									<div class=\"container\">
+										<div class=\"page-header\">
+											<h3 class=\"text-center\">
+												".get_project_name()." : Réinitialiser mon mot de passe
+											</h3>
+										</div>
+										<h3 class=\"text-center\">
+											Votre code de sécurité : 
+										</h3>
+										<h3 class=\"text-center\">
+											<kbd>".$code."</kbd>
+										</h3><br>
+										<h3 class=\"text-center\">
+											<a href=\"".get_root_url().get_base_url()."forget&token=".$token."\" target=\"_blank\">Valider ici</a>
+										</h3>
+									</div>
+								</body>
+							</html>";
+				mail($to, $subject, $content, $headers);
+				$msg = 'Un email vous a été envoyé, veuillez suivre les instructions de ce dernier';
+				display_confirm_email_form('success', $msg, $style);
 			}else{
 				$error = 'adresse email incorrecte';
+				display_confirm_email_form('danger', $error, $style);
 			}
 		}else{
-			$error = 'adresse email incorrecte';
+			if(!isset($_SESSION['attempts'])){
+				$_SESSION['attempts'] = 0;
+			}
 			$_SESSION['attempts']++;
+			$diff = $max_attempts - $_SESSION['attempts'];
+			if($diff > 0){
+				if($diff === 1){
+					$text = 'essai';
+				}else{
+					$text = 'essais';
+				}
+				$error = 'adresse email incorrecte, il vous reste '.$diff.' '.$text;
+			}else{
+				$bans = get_ban_duration_list();
+				$error = 'adresse email incorrecte, vous êtes banni pour une durée de '.$bans[0][0];
+				$ban_level = 0;
+				$user_ip = get_user_ip();
+				ban(NULL, 'Ban automatique', $user_ip, $ban_level, NULL);
+				$_SESSION['attempts'] = 0;
+			}
+			display_confirm_email_form('danger', $error, $style);
 		}
 	}else{
 		$error = 'adresse email invalide';
+		display_confirm_email_form('danger', $error, $style);
 	}
+}else if(isset($_GET['token']) && !empty($_GET['token']) && is_string($_GET['token'])){
+	$token = $_GET['token'] = secure($_GET['token']);
+	if(!is_token($token)){
+		if(!isset($_SESSION['attempts'])){
+			$_SESSION['attempts'] = 0;
+		}
+		$_SESSION['attempts']++;
+		$diff = $max_attempts - $_SESSION['attempts'];
+		$ban_level = 0;
+		if($diff === 0){
+			$user_ip = get_user_ip();
+			ban(NULL, 'Ban automatique', $user_ip, $ban_level, NULL);
+			$_SESSION['attempts'] = 0;
+			set_error('Erreur', 'zoom-out', 'Token invalide, veuillez ne pas réessayer', 'forget');
+		}else{
+			set_error('Erreur', 'zoom-out', 'Token invalide, veuillez ne pas réessayer', 'forget');
+		}
+	}
+	if(isset($_POST['submit_code'])){
+		if(isset($_POST['code']) && !empty($_POST['code']) && is_string($_POST['code'])){
+			$code = $_POST['code'] = secure($_POST['code']);
+			$name = check_codes($token, $code);
+			if($name != NULL){
+				$_SESSION['tmp_name'] = $name;
+				$_SESSION['code_validate'] = 1;
+				$_SESSION['token_validate'] = 1;
+				display_new_password_form();
+			}
+		}
+	}else if(isset($_POST['submit_password'])){
+		if($_SESSION['code_validate'] && $_SESSION['token_validate']){
+			if(isset($_POST['new_password'], $_POST['repeat_new_password']) && !empty($_POST['new_password']) && 
+				!empty($_POST['repeat_new_password']) && is_string($_POST['new_password']) && is_string($_POST['repeat_new_password'])){
+				$new_password = $_POST['new_password'] = secure($_POST['new_password']);
+				$repeat_new_password = $_POST['repeat_new_password'] = secure($_POST['repeat_new_password']);
+				update_passwords($new_password, $repeat_password);
+			}else{
+				$error = 'Veuillez saisir votre nouveau mot de passe';
+			}
+		}else{
+			set_error('Erreur', 'exclamation-sign', 'Erreur lors du processus de réinitialisation, veuillez suivre les instructions', 'forget');
+		}
+	}else{
+		display_confirm_code_form();
+	}
+}else{
+	display_confirm_email_form('danger', $error, $style);
 }
-?>
-<div class="page-header">
-	<h3 class="text-center">Mot de passe oublié</h3>
-</div>
-<div class="col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3">
-<?php
-	
-	if(!empty($error)){
-		echo "<div class=\"alert alert-danger text-center\">
-				<span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span> ".$error."
-			</div>";
-	}
-	if(!empty($style)){
-		echo "<div class=\"alert alert-danger invisible\">".$style."</div>";
-	}
-?>
-</div>
-<div class="col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title">
-				<strong>Réinitialiser mon mot de passe</strong>
-			</h3>
-		</div>
-		<div class="panel-body">
-			<form method="POST">
-				<div class="form-group">
-					<label>Votre adresse email :</label>
-					<input type="email" class="form-control" name="email" maxlength="40" 
-						placeholder="exemple@exemple.com" autofocus required>
-				</div>
-				<button class="btn btn-primary" name="submit">Confirmer</button>
-				<a class="btn btn-default" href="<?= get_root_url().get_base_url() ?>login">
-					Annuler
-				</a>
-			</form>
-		</div>
-	</div>
-</div>
