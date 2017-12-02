2cd7077acf2353607342d55b667c9ba74972e790
f1sher <f1sher@gmx.com>
2017-12-02 16:41:50 +0100

    Minor BUG fixing : now displaying whole article after comment posted

--- pages/article.php
+++ pages/article.php
@@ -59,7 +59,7 @@ if(isset($_GET['id']) && !empty($_GET['id']) && ctype_digit($_GET['id'])){
 		if(isset($_POST['comment']) && !empty($_POST['comment']) && is_string($_POST['comment']) && mb_strlen($_POST['comment']) <= 500){
 				$comment = $_POST['comment'] = secure($_POST['comment']);
 				post_comment($id, $_SESSION['name'], $comment, $reply_to);
-				display_comment(false, $id, $pid);
+				display_article($id, $pid);
 		}else if($pid != 1){
 			display_comment(false, $id, $pid);
 		}else{
