b05752fccb26bd7e8ae2b71d5c95f441dbe1f973
f1sher <f1sher@gmx.com>
2017-12-03 16:11:05 +0100

    Minor BUG fixing and changes

--- www/pages/error404.php
+++ www/pages/error404.php
@@ -1,3 +1,7 @@
 <?php
 
-set_error('Erreur 404', 'zoom-out', 'La page à laquelle vous tentez d\'accéder n\'existe pas', 'home');
+if(is_logged()){
+	set_error('Erreur 404', 'zoom-out', 'La page à laquelle vous tentez d\'accéder n\'existe pas', 'home');
+}else{
+	set_error('Erreur 404', 'zoom-out', 'La page à laquelle vous tentez d\'accéder n\'existe pas', 'login');
+}
