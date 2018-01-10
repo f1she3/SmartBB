71a966463fe03af83b647f7df47f7c207eebcd16
f1sher <f1sher@gmx.com>
2018-01-10 18:13:34 +0100

    Removed old project's description, added project's description

--- www/pages/welcome.php
+++ www/pages/welcome.php
@@ -1,44 +1,11 @@
 <div class="page-header">
-	<h1 class="text-center">Bienvenue sur <?= get_project_name() ?> !</h1>
+	<h1 class="text-center">Welcome to <?= get_project_name() ?> !</h1>
 </div>
-<p class="lead text-center">
-	Nous sommes heureux de vous accueillir sur <?= get_project_name() ?> !<br>
-</p>
-<p class="text-justify">
-<?= get_project_name() ?> est un <b>forum francophone de <a
-   href="https://fr.wikipedia.org/wiki/Cybers%C3%A9curit%C3%A9" target="_blank">cyber-sécurité</a></b>
-	où vous pouvez apprendre et partager sur différents sujets, liés de
-	près ou de loin à l'informatique et à la cyber-sécurité, avec d'autres
-	membres passionnés. <?= get_project_name() ?> a pour but de créer une communauté de
-	passionnés où l'on partage et s'entraide afin de permettre à tout le
-	monde de progresser, chacun à son niveau.
-</p>
-<p class="text-justify">
-<b>
-	<u>Important</u> :
-</b>
-	Afin d'éviter toute présence de publicité et de trackers dégradant la
-	navigation sur le site, nous vous demandons une participation de
-	<b><u>1€</u></b> à l'inscription. Cette petite condition
-	nous permet de faire vivre le projet en restant autonome et libres, ce
-	qui est fondamental. Cette participation est simplement effectuée en bitcoin
-	directement lors de l'inscription.<br>
-</p>
-<p class="text-justify">
-<b>
-	<u>Nous soutenir</u> :
-</b>
-	Le projet fonctionne grâce au bouche à oreille, n'hésitez donc pas à en
-	parler autour de vous et à montrer  à vos amis, c'est le meilleur moyen de
-	nous soutenir <img src="../css/images/emojis/e_c.svg" height="18"
-						       width="18">
-	(Vous pouvez également nous faire <a href="#">un
-		don</a>).<br>
-	<p class="text-center"><b>Merci de votre compréhension et de votre
-		générosité !</b></p>
-</p>
 <p class="lead text-justify">
-	Nous vous souhaitons une bonne navigation et un bon
-	partage avec les autres membres, 
-</h3><br>
-<h3><u><?= get_project_name() ?></u></h3>
+	<?= get_project_name() ?> is a forum system that aims to be portable and configurable by developpers
+	in order to create your own forum.<br>
+	You will find everything you need in the file <kbd>functions/init.php</kbd><br>
+	Feel free to comment and report any problem ;)
+	
+</p>
+<h3><u>By <a href="https://github.com/f1she3">f1she3</a></u></h3>
